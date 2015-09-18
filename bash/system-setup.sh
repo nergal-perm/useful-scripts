c1595dfcf92bc5095dd189a5c81602e6f554496b
Eugene Terekhov <nergal@psu.ru>
2015-09-18 20:43:06 +0500

    fixed a bug with rescuetime filename

--- bash/system-setup.sh
+++ bash/system-setup.sh
@@ -14,8 +14,8 @@ sudo python setup.py install
 cd ~/Downloads
 sudo apt-get -y install curl
 curl -O https://www.rescuetime.com/installers/rescuetime_current_i386.deb
-sudo dpkg -i rescuetime_current_i386
+sudo dpkg -i rescuetime_current_i386.deb
 sudo apt-get -y -f install
 mkdir ~/.config/autostart
-
+curl https://raw.githubusercontent.com/nergal-perm/useful-scripts/master/bash/rescuetime.desktop >> ~/.config/autostart/rescuetime.desktop
 
