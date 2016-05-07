bd338b3fa62449fece22ba078f7d0691b66af61c
nergal-perm <nergal@psu.ru>
2016-05-07 09:08:08 +0500

    Added sublime-text packages installation

--- bash/sublime-setup.sh
+++ bash/sublime-setup.sh
@@ -6,3 +6,6 @@ cd ~/Загрузки
 wget -q https://download.sublimetext.com/sublime-text_build-3103_i386.deb
 sudo dpkg -i sublime-text_build-3103_i386.deb
 sudo apt-get -y -f install
+
+git clone https://github.com/jonschlinkert/sublime-monokai-extended ~/.config/sublime-text-3/Packages/sublime-monokai-extended
+git clone https://github.com/jonschlinkert/sublime-markdown-extended ~/.config/sublime-text-3/Packages/sublime-markdown-extended
