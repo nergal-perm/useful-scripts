41f051311d591d8165837a0ab39f0da7dfa0cc1d
nergal-perm <nergal@psu.ru>
2020-01-27 18:03:13 +0500

    Updated random wiki note selection

--- bash/vim-notes.sh
+++ bash/vim-notes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-random_note=`find $PERSONAL_WIKI -type f | sort -R | head -1`
+#random_note=`find #PERSONAL_WIKI -type f -iname "*.md" | sort -R | head -1`
+random_note=`find $PERSONAL_WIKI -type f -regextype egrep ! -regex ".*/20[0-9]{10}.*\.md" | sort -R | head -1`
 (cd $PERSONAL_WIKI && vim "${random_note}")
