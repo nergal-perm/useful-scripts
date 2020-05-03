ba0cb5d8864424a32c98e7e826cd95c0182986c7
nergal-perm <nergal@psu.ru>
2020-05-03 05:58:48 +0500

    Added repo cloning and symlink creation

--- bash/vi-setup.sh
+++ bash/vi-setup.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-sudo apt-get update
-sudo install vim
+# Install prerequisite packages
+sudo apt update && sudo apt install git curl vim
+
+# Install vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Get dotfiles repository
+git clone git@github.com:nergal-perm/dotfiles.git ~/Dev/dotfiles
+rm ~/.vimrc
+ln -f -s ~/Dev/dotfiles/.vimrc ~/.vimrc
+
